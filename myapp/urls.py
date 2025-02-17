694c591123580e6434fd1eecd7f499b8ad71ab5b
Raj <asrajendrayadav@gmail.com>
2025-02-17 22:06:02 +0530

    project structure created

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login, name="login"),
+    path("sign_up", views.sign_up, name="sign_up"),
+    path("get_naani", views.get_naani, name="get_naani"),
+    path("cry_detection", views.cry_detection, name="cry_detection"),
+    path("parenting_tips", views.parenting_tips, name="parenting_tips"),
+    path("discussion_forum", views.discussion_forum, name="discussion_forum"),
+    path("telehealth", views.parenting_tips, name="telehealth"),
+    path("memory_book", views.memory_book, name="memory_book"),
+    path("growth_track", views.growth_track, name="growth_track"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("profile", views.profile, name="profile"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,40 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hey, Welcome to BabyCare")
+  return render(request, "index.html")
+
+def login(request):
+  return render(request, "login.html")
+
+def sign_up(request):
+  return render(request, "sign_up.html")
+
+def cry_detection(request):
+  return render(request, "cry_detection.html")
+
+def get_naani(request):
+  return render(request, "get_naani.html")
+
+def parenting_tips(request):
+  return render(request, "parenting_tips.html")
+
+def discussion_forum(request):
+  return render(request, "discussion_forum.html")
+
+def telehealth(request):
+  return render(request, "telehealth.html")
+
+def memory_book(request):
+  return render(request, "memory_book.html")
+
+def growth_track(request):
+  return render(request, "growth_track.html")
+
+def about(request):
+  return render(request, "about.html")
+
+def profile(request):
+  return render(request, "profile.html")
+
+def contact(request):
+  return render(request, "contact.html")
